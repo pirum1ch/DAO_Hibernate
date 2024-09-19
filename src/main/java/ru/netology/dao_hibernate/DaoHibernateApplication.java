eaf514c872de93424ac662e47607181ca7e8ee27
dmitriy.pirumov <dmitry.pirumov@gmail.com>
2024-09-19 19:50:41 +0300

    init commit

---
+++ src/main/java/ru/netology/dao_hibernate/DaoHibernateApplication.java
@@ -0,0 +1,13 @@
+package ru.netology.dao_hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaoHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaoHibernateApplication.class, args);
+    }
+
+}
